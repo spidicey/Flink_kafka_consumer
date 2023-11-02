2ba966950e06d32c819c0a9b0a6e1028553bda0b
spidicey <82193435+spidicey@users.noreply.github.com>
2023-11-02 11:14:13 +0700

    first commit

---
+++ src/main/java/com/flink/flinkTest/FlinkTestApplication.java
@@ -0,0 +1,14 @@
+package com.flink.flinkTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(FlinkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flink/flinkTest/FlinkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flink.flinkTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
